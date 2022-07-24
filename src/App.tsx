53399537158f0cf42111f9db7848b7b2e6f9bb1a
Natalie Tang <natalietang77@gmail.com>
2022-07-24 13:02:24 -0500

    created screens for welcome, about me, & contact me

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import Welcome from './screens/welcome'
+import AboutMe from './screens/aboutme'
+import ContactMe from './screens/contactme'
 function App() {
   return (
     <div>
-     <h1>Hello world</h1>
+     <h1>Hello world</h1> 
+     <Welcome />
+     <AboutMe />
+     <ContactMe />
     </div>
   );
 }
